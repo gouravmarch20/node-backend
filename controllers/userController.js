d9aba1c6704bc26a0b242c5365d028e7c0598003
gouravmarch20 <gouravmarch20@gmail.com>
2021-12-20 19:48:56 +0530

    add  feat :  admin user , updatePassword

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,9 +9,9 @@ const crypto = require("crypto");
 exports.signup = BigPromise(async (req, res, next) => {
     let result;
     // console.log(req.files)
-    if (!req.files) {
-        return next(new CustomError("photo is required for signup", 400));
-    }
+    // if (!req.files) {
+    //     return next(new CustomError("photo is required for signup", 400));
+    // }
 
     const { name, email, password } = req.body;
 
@@ -173,4 +173,182 @@ exports.passwordReset = BigPromise(async (req, res, next) => {
     // send a JSON response OR send token
 
     cookieToken(user, res);
+});
+exports.getLoggedInUserDetails = BigPromise(async (req, res, next) => {
+    //req.user will be added by middleware
+    // find user by id
+    const user = await User.findById(req.user.id);
+
+    //send response and user data
+    res.status(200).json({
+        success: true,
+        user,
+    });
+});
+
+exports.changePassword = BigPromise(async (req, res, next) => {
+    // get user from middleware
+    const userId = req.user.id;
+
+    // get user from database
+    const user = await User.findById(userId).select("+password");
+
+    //check if old password is correct
+    const isCorrectOldPassword = await user.isValidatedPassword(
+        req.body.oldPassword
+    );
+
+    if (!isCorrectOldPassword) {
+        return next(new CustomError("old password is incorrect", 400));
+    }
+
+    // allow to set new password
+    user.password = req.body.password;
+
+    // save user and send fresh token
+    await user.save();
+    cookieToken(user, res);
+});
+
+exports.updateUserDetails = BigPromise(async (req, res, next) => {
+
+    const { name } = req.body
+    // add a check for email and name in body
+    if (!name) {
+        return next(new CustomError("name must required to update", 400));
+    }
+    // collect data from body
+    const newData = {
+        name: req.body.name,
+        email: req.body.email,
+        role: req.body.role
+    };
+
+    // if photo comes to us
+    if (req.files) {
+        const user = await User.findById(req.user.id);
+
+        const imageId = user.photo.id;
+
+        // delete photo on cloudinary
+        const resp = await cloudinary.v2.uploader.destroy(imageId);
+
+        // upload the new photo
+        const result = await cloudinary.v2.uploader.upload(
+            req.files.photo.tempFilePath,
+            {
+                folder: "users",
+                width: 150,
+                crop: "scale",
+            }
+        );
+
+        // add photo data in newData object
+        newData.photo = {
+            id: result.public_id,
+            secure_url: result.secure_url,
+        };
+    }
+
+    // update the data in user
+    const user = await User.findByIdAndUpdate(req.user.id, newData, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false,
+    });
+
+    res.status(200).json({
+        success: true,
+    });
+});
+// ! all user display
+exports.adminAllUser = BigPromise(async (req, res, next) => {
+    // select all users
+    const users = await User.find();
+
+    // send all users
+    res.status(200).json({
+        success: true,
+        users,
+    });
+});
+
+exports.admingetOneUser = BigPromise(async (req, res, next) => {
+    // get id from url and get user from database
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+        next(new CustomError("No user found", 400));
+    }
+
+    // send user
+    res.status(200).json({
+        success: true,
+        user,
+    });
+});
+
+exports.adminUpdateOneUserDetails = BigPromise(async (req, res, next) => {
+    // add a check for email and name in body
+
+    const { name } = req.body
+
+    if (!name) {
+        next(new CustomError("Name  must require ", 400));
+
+    }
+    // get data from request body
+    const newData = {
+        name: req.body.name,
+        email: req.body.email,
+        role: req.body.role,
+    };
+
+    // update the user in database
+    const user = await User.findByIdAndUpdate(req.params.id, newData, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false,
+    });
+
+    res.status(200).json({
+        success: true,
+    });
+});
+
+exports.adminDeleteOneUser = BigPromise(async (req, res, next) => {
+    // get user from url
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+        return next(new CustomError("No Such user found", 401));
+    }
+    console.log(req.user.photo.secure_url)
+
+    // if user upload photo : destory it
+    // if (req.user.photo.photo?.url) {
+        // get image id from user in database
+        const imageId = user.photo.id;
+
+        // delete image from cloudinary
+        await cloudinary.v2.uploader.destroy(imageId);
+
+    // }
+
+    // remove user from databse
+    await user.remove();
+
+    res.status(200).json({
+        success: true,
+    });
+});
+
+exports.managerAllUser = BigPromise(async (req, res, next) => {
+    // select the user with role of user
+    const users = await User.find({ role: "user" });
+
+    res.status(200).json({
+        success: true,
+        users,
+    });
 });
--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const router = express.Router();
 
-const { signup, signin, signout,forgotPassword,passwordReset } = require("../controllers/userController")
+const { signup, signin, signout, forgotPassword, passwordReset, getLoggedInUserDetails, changePassword, updateUserDetails,adminAllUser, admingetOneUser, adminUpdateOneUserDetails, adminDeleteOneUser } = require("../controllers/userController")
+const { isLoggedIn, customRole } = require("../middlewares/user");
+
+
+
 router.route("/signup").post(signup);
 router.route("/signin").post(signin);
 router.route("/signout").get(signout);
 router.route("/forgotPassword").post(forgotPassword);
 router.route("/password/reset/:token").post(passwordReset);
+router.route("/userdashboard").get(isLoggedIn, getLoggedInUserDetails);
+router.route("/password/update").post(isLoggedIn, changePassword);
+router.route("/userdashboard/update").post(isLoggedIn, updateUserDetails);
+//admin only routes
+router.route("/admin/users").get(isLoggedIn, customRole("admin"), adminAllUser);
+router
+    .route("/admin/user/:id")
+    .get(isLoggedIn, customRole("admin"), admingetOneUser)
+    .put(isLoggedIn, customRole("admin"), adminUpdateOneUserDetails)
+    .delete(isLoggedIn, customRole("admin"), adminDeleteOneUser);
 
 module.exports = router;
